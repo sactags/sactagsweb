2e086d00d12255f30ae356ff263e1415bc475ca2
Eerpina <srinivas.eerpina@intel.com>
2018-09-15 17:13:15 -0700

    hi

---
+++ git-bash.cmd
@@ -0,0 +1,6 @@
+"C:\Program Files\Git\git-bash.exe" git init
+"C:\Program Files\Git\git-bash.exe" git pull https://github.com/sactags/sactagsweb.git master
+"C:\Program Files\Git\git-bash.exe" git add .
+"C:\Program Files\Git\git-bash.exe -m "message"
+"C:\Program Files\Git\git-bash.exe" push https://github.com/sactags/sactagsweb.git master
+pause
